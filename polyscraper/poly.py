093780b47da4ae626bbdb9ae494161cc5404e48b
Nathaniel Case <Qalthos@gmail.com>
2013-03-18 23:08:15 -0700

    Remove long broken and surpassed doctest.

--- polyscraper/poly.py
+++ polyscraper/poly.py
@@ -79,51 +79,13 @@ from knowledge.model import Fact, Entity, DBSession
 
 from bs4 import BeautifulSoup
 
+from polyscraper import utils
 from polyscraper.scraper import Scraper
 
 extensions = u'csv,zip,exe,xls,txt,rss,xml,json'
 
 class PolyScraper(Scraper):
-    """ The CIVX Polymorphic Scraper.
-
-    >>> url = u'http://www.data.gov/raw/994'
-    >>> poly = PolyScraper()
-    >>> poly.consume({'body': {'url': url}})
-    >>> entity = Entity.by_name('data.gov')
-    >>> len(entity.children)
-    1
-    >>> child = entity.children.values()[0]
-    >>> child.name
-    u'Department of Commerce'
-    >>> child = child.children.values()[0]
-    >>> child.name
-    u'Denied Persons List with Denied US Export Privileges'
-    >>> child[u'repo']
-    u'data.gov'
-    >>> child[u'Agency']
-    u'Department of Commerce'
-    >>> len(child.children)
-    2
-    >>> child = child.children.values()[1]
-    >>> assert u'table_name' in child.facts
-    >>> assert u'column_names' in child.facts
-    >>> assert u'columns' in child.facts
-    >>> assert child[u'filename'].endswith('csv')
-    >>> table, model = get_mapped_table_model_from_entity(child)
-    >>> int(DBSession.query(model).count())
-    417
-    >>> DBSession.query(model)
-    http://data.gov/download/994/csv
-     * Name: A. ROSENTHAL (PTY) LTD.
-     * Street_Address: P.O. BOX 44198, 65 7TH STREET, DENMYR BUILDING
-     * City: LINDEN
-     * State:
-     * Country: ZA
-     * Postal_Code: 2104
-     * Effective_Date: 08/08/1997
-     * Expiration_Date: 08/08/2017
-     * Standard_Order: Y
-    """
+    """ The CIVX Polymorphic Scraper. """
 
     def __init__(self):
         self.log = logging.getLogger('PolyScraper')
